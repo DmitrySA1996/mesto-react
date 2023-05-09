c6f00001f0a7a5a29ea84fab680be3f7b45850d7
Dmitry Senchenko <114693652+DmitrySA1996@users.noreply.github.com>
2023-05-09 20:54:21 +0300

    Add files via upload

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import "./App.js";
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__copyright">© {new Date().getFullYear()} Mesto Russia</p>
+      <p className="footer__copyright">© 2022-{new Date().getFullYear()} Mesto Russia</p>
     </footer>
   );
 }
